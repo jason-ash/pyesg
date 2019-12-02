5ab40e5fe32f95e96ad13dee542cd90861a4cf33
Jason Ash <ash.jasont@gmail.com>
2019-12-02 13:51:03 -0800

    refactoring stochastic models (#25)
    * refactoring stochastic models
    
    * add geometric brownian motion
    
    * move mu and sigma properties to base class
    
    * remove diffusion_process.py
    
    * update __init__; small changes to models.py
    
    * update tests
    
    * review pylintrc

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,105 +1,111 @@
 """Tests for the Vasicek Model"""
+import doctest
 import unittest
 from hypothesis import given, settings
 from hypothesis.strategies import floats, integers
 import numpy as np
 
-from pyesg import CoxIngersollRoss, GeometricBrownianMotion, Vasicek
+from pyesg import models
+from pyesg import WienerProcess, OrnsteinUhlenbeckProcess, GeometricBrownianMotion
 
 
-class TestVasicek(unittest.TestCase):
-    """Test Vasicek Model"""
+# pylint: disable=unused-argument,line-too-long
+def load_tests(loader, tests, ignored):
+    """
+    This function allows unittest to discover doctests in the module.
+
+    It appears not to use the arguments (or do anything, really), but this is
+    used internally by unittest to "discover" the tests it needs to run.
+
+    References
+    ----------
+    https://stackoverflow.com/questions/5681330/using-doctests-from-within-unittests
+    https://docs.python.org/2/library/unittest.html#load-tests-protocol
+    """
+    tests.addTests(doctest.DocTestSuite(models))
+    return tests
+
+
+class TestWienerProcess(unittest.TestCase):
+    """Test WienerProcess"""
 
     @settings(deadline=None)
     @given(n_scen=integers(1, 100), n_year=integers(1, 20), n_step=integers(1, 252))
     def test_sample_shapes(self, n_scen, n_year, n_step):
         """Ensure samples have the correct shape"""
-        model = Vasicek()
-        model.k, model.theta, model.sigma = 0.15, 0.045, 0.015
+        model = WienerProcess()
+        model.mu, model.sigma = 0.045, 0.015
         samples = model.sample(0.03, n_scen, n_year, n_step, random_state=None)
         self.assertEqual(samples.shape, (n_scen, 1 + n_year * n_step))
 
     @given(init=floats(0, allow_infinity=False))
     def test_sample_init_value1(self, init):
         """Ensure the init value is used as the scenario start value"""
-        model = Vasicek()
-        model.k, model.theta, model.sigma = 0.15, 0.045, 0.015
+        model = WienerProcess()
+        model.mu, model.sigma = 0.045, 0.015
         samples = model.sample(init, 10, 10, 1, random_state=None)
         self.assertListEqual(list(samples[:, 0]), [init] * 10)
 
     @given(init=floats(0, allow_infinity=False))
     def test_sample_init_value2(self, init):
         """Ensure the init value is used as the scenario start value"""
-        model = Vasicek()
-        model.k, model.theta, model.sigma = 0.15, 0.045, 0.015
+        model = WienerProcess()
+        model.mu, model.sigma = 0.045, 0.015
         samples = model.sample(np.full(10, init), 10, 10, 1, random_state=None)
         self.assertListEqual(list(samples[:, 0]), [init] * 10)
 
     def test_raises_value_error(self):
         """Ensure we raise a ValueError if init shape doesn't match n_scen"""
-        model = Vasicek()
-        model.k, model.theta, model.sigma = 0.15, 0.045, 0.015
+        model = WienerProcess()
+        model.mu, model.sigma = 0.045, 0.015
         init = np.array([0.03, 0.03])
         self.assertRaises(ValueError, model.sample, init, 1000, 30, 12)
 
 
-class TestCoxIngersollRoss(unittest.TestCase):
-    """Test Cox-Ingersoll-Ross Model"""
+class TestOrnsteinUhlenbeckProcess(unittest.TestCase):
+    """Test OrnsteinUhlenbeckProcess"""
 
     @settings(deadline=None)
     @given(n_scen=integers(1, 100), n_year=integers(1, 20), n_step=integers(1, 252))
     def test_sample_shapes(self, n_scen, n_year, n_step):
         """Ensure samples have the correct shape"""
-        model = CoxIngersollRoss()
-        model.k, model.theta, model.sigma = 0.10, 0.045, 0.015
+        model = OrnsteinUhlenbeckProcess()
+        model.theta, model.mu, model.sigma = 0.15, 0.045, 0.015
         samples = model.sample(0.03, n_scen, n_year, n_step, random_state=None)
         self.assertEqual(samples.shape, (n_scen, 1 + n_year * n_step))
 
     @given(init=floats(0, allow_infinity=False))
     def test_sample_init_value1(self, init):
         """Ensure the init value is used as the scenario start value"""
-        model = CoxIngersollRoss()
-        model.k, model.theta, model.sigma = 0.10, 0.045, 0.015
+        model = OrnsteinUhlenbeckProcess()
+        model.theta, model.mu, model.sigma = 0.15, 0.045, 0.015
         samples = model.sample(init, 10, 10, 1, random_state=None)
         self.assertListEqual(list(samples[:, 0]), [init] * 10)
 
     @given(init=floats(0, allow_infinity=False))
     def test_sample_init_value2(self, init):
         """Ensure the init value is used as the scenario start value"""
-        model = CoxIngersollRoss()
-        model.k, model.theta, model.sigma = 0.10, 0.045, 0.015
+        model = OrnsteinUhlenbeckProcess()
+        model.theta, model.mu, model.sigma = 0.15, 0.045, 0.015
         samples = model.sample(np.full(10, init), 10, 10, 1, random_state=None)
         self.assertListEqual(list(samples[:, 0]), [init] * 10)
 
     def test_raises_value_error(self):
         """Ensure we raise a ValueError if init shape doesn't match n_scen"""
-        model = CoxIngersollRoss()
-        model.k, model.theta, model.sigma = 0.10, 0.045, 0.015
+        model = OrnsteinUhlenbeckProcess()
+        model.theta, model.mu, model.sigma = 0.15, 0.045, 0.015
         init = np.array([0.03, 0.03])
         self.assertRaises(ValueError, model.sample, init, 1000, 30, 12)
 
 
 class TestGeometricBrownianMotion(unittest.TestCase):
-    """Test Cox-Ingersoll-Ross Model"""
+    """Test GeometricBrownianMotion"""
 
     @settings(deadline=None)
     @given(n_scen=integers(1, 100), n_year=integers(1, 20), n_step=integers(1, 252))
     def test_sample_shapes(self, n_scen, n_year, n_step):
         """Ensure samples have the correct shape"""
-        model = GeometricBrownianMotion(n_indices=3)
-        model.correlation = np.array(
-            [[1.0, -0.19197, 0.0], [-0.19197, 1.0, 0.0], [0.0, 0.0, 1.0]]
-        )
+        model = GeometricBrownianMotion()
         model.mu, model.sigma = 0.05, 0.20
         samples = model.sample(100.0, n_scen, n_year, n_step, random_state=None)
-        self.assertEqual(samples.shape, (n_scen, 1 + n_year * n_step, 3))
-
-    def test_coef_array(self):
-        """Ensure the sample has the correct shape if coefs are passed as arrays"""
-        model = GeometricBrownianMotion(n_indices=3)
-        model.mu, model.sigma = np.full(3, 0.05), np.full(3, 0.20)
-        model.correlation = np.array(
-            [[1.0, -0.19197, 0.0], [-0.19197, 1.0, 0.0], [0.0, 0.0, 1.0]]
-        )
-        samples = model.sample(0.03, 1000, 30, 12, random_state=None)
-        self.assertEqual(samples.shape, (1000, 30 * 12 + 1, 3))
+        self.assertEqual(samples.shape, (n_scen, 1 + n_year * n_step))
