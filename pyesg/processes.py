b16a699cb89fd22c8b15fea0237d6532353867ce
Jason Ash <ash.jasont@gmail.com>
2019-12-03 17:36:38 -0800

    add geometric brownian motion process

--- pyesg/processes.py
+++ pyesg/processes.py
@@ -59,7 +59,7 @@ class StochasticProcess(ABC):
         """
         return self.diffusion(x0=x0) * dt ** 0.5
 
-    def transition_dist(self, x0: Vector, dt: float) -> rv_frozen:
+    def transition_distribution(self, x0: Vector, dt: float) -> rv_frozen:
         """
         Returns a calibrated scipy.stats distribution object for the transition, given
         a starting value, x0
@@ -73,7 +73,7 @@ class StochasticProcess(ABC):
         Returns the log-probability of moving from x0 to x1 starting at time t and
         moving to time t + dt
         """
-        return self.transition_dist(x0=x0, dt=dt).logpdf(xt)
+        return self.transition_distribution(x0=x0, dt=dt).logpdf(xt)
 
     def nnlf(self, x0: Vector, xt: Vector, dt: float) -> Vector:
         """
@@ -175,6 +175,46 @@ class OrnsteinUhlenbeckProcess(StochasticProcess):
     def diffusion(self, x0: Vector) -> Vector:
         # diffusion of an Ornstein-Uhlenbeck process does not depend on x0
         return self.sigma
+
+
+class GeometricBrownianMotion(StochasticProcess):
+    """
+    Geometric Brownian Motion process: dX = μXdt + σXdW
+
+    Examples
+    --------
+    >>> gbm = GeometricBrownianMotion(mu=0.05, sigma=0.2)
+    >>> gbm
+    <pyesg.GeometricBrownianMotion{'mu': 0.05, 'sigma': 0.2}>
+    >>> gbm.drift(x0=2.0)
+    0.1
+    >>> gbm.diffusion(x0=2.0)
+    0.4
+    >>> gbm.expectation(x0=1.0, dt=0.5)
+    1.025
+    >>> gbm.standard_deviation(x0=1.0, dt=0.5)
+    0.14142135623730953
+    >>> gbm.step(x0=1.0, dt=1.0, random_state=42)
+    array([1.14934283])
+    >>> gbm.step(x0=np.array([1.0, 15.0, 50.0]), dt=1.0, random_state=42)
+    array([ 1.14934283, 15.3352071 , 58.97688538])
+    >>> gbm.logpdf(x0=1.0, xt=1.1, dt=1.0)
+    0.6592493792294276
+    """
+
+    def __init__(self, mu: Vector, sigma: Vector) -> None:
+        super().__init__()
+        self.mu = mu
+        self.sigma = sigma
+
+    def coefs(self) -> Dict[str, Vector]:
+        return dict(mu=self.mu, sigma=self.sigma)
+
+    def drift(self, x0: Vector) -> Vector:
+        return self.mu * x0
+
+    def diffusion(self, x0: Vector) -> Vector:
+        return self.sigma * x0
 
 
 if __name__ == "__main__":
