5e7bb0327c3c43f3f90fa2d0dd207aab1a7b761a
Jason Ash <ash.jasont@gmail.com>
2019-12-11 14:22:05 -0800

    add back to_array in apply

--- pyesg/stochastic_process.py
+++ pyesg/stochastic_process.py
@@ -63,7 +63,7 @@ class StochasticProcess(ABC):
 
     def apply(self, x0: Array, dx: np.ndarray) -> np.ndarray:
         """Returns a new array of x-values, given a starting array and change vector"""
-        return self._apply(x0=to_array(x0), dx=dx)
+        return self._apply(x0=to_array(x0), dx=to_array(dx))
 
     def drift(self, x0: Array) -> np.ndarray:
         """Returns the drift component of the stochastic process"""
