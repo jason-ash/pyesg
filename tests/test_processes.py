d7ab4927c2889e1b1e22d7c7fd41819ddc977f21
Jason Ash <ash.jasont@gmail.com>
2019-12-10 16:10:25 -0800

    add failing tests for #41

--- tests/test_processes.py
+++ tests/test_processes.py
@@ -3,7 +3,7 @@ import doctest
 import unittest
 import numpy as np
 
-from pyesg import WienerProcess
+from pyesg import AcademyRateProcess, WienerProcess
 from pyesg.processes import (
     academy_rate_process,
     cox_ingersoll_ross_process,
@@ -52,3 +52,19 @@ class TestWienerProcess(unittest.TestCase):
         self.assertEqual(steps.shape, (1,))
         steps = model.step(x0=np.full(10, 0.05), dt=1.0, random_state=None)
         self.assertEqual(steps.shape, (10,))
+
+
+class TestAcademyRateProcess(unittest.TestCase):
+    """Test AcademyRateProcess"""
+
+    def test_cap(self):
+        """Ensure that the drift process produces values within the acceptable range"""
+        arp = AcademyRateProcess(long_rate_min=0.0115, long_rate_max=0.18)
+
+        # ensure the expectation is never higher than the long_rate_max
+        x0 = np.array([0.2, 0.0024, 0.03])
+        self.assertLessEqual(arp.expectation(x0=x0, dt=1.0)[0], 0.018)
+
+        # ensure the expectatin is never lower than the long_rate_min
+        x0 = np.array([0.005, 0.0024, 0.03])
+        self.assertGreaterEqual(arp.expectation(x0=x0, dt=1.0)[0], 0.0115)
