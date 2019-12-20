0652421aafd216ccaa64b10513bf3eaad8bfdc31
Jason Ash <ash.jasont@gmail.com>
2019-12-19 16:30:14 -0800

    add process to top level imports

--- pyesg/__init__.py
+++ pyesg/__init__.py
@@ -1,6 +1,8 @@
 """Top level imports"""
 from pyesg.interpolators import NelsonSiegel, NelsonSiegelSvensson
 from pyesg.processes.academy_rate_process import AcademyRateProcess
+from pyesg.processes.black_scholes_process import BlackScholesProcess
+from pyesg.processes.cox_ingersoll_ross_process import CoxIngersollRossProcess
 from pyesg.processes.geometric_brownian_motion import GeometricBrownianMotion
 from pyesg.processes.heston_process import HestonProcess
 from pyesg.processes.ornstein_uhlenbeck_process import OrnsteinUhlenbeckProcess
